3bc0ca51be3e20b4532fee565feef290f1fa9dd7
Daria Yakkola <yakkolabest@gmail.com>
2023-02-12 17:25:28 +0300

    Задача 4

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Введите первое число: ");
+int numbers1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numbers2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int numbers3 = Convert.ToInt32(Console.ReadLine());
+if (numbers1 > numbers2)
+{ 
+  if (numbers1 > numbers3)
+  {
+    Console.WriteLine("Наибольшее число будет: " + numbers1);
+  }
+  else
+  {
+    Console.WriteLine("Наибольшее число будет: " + numbers3);
+  }
+}
+else
+{
+  if (numbers2 > numbers3)
+  {
+    Console.WriteLine("Наибольшее число будет: " + numbers2);
+  }
+  else
+  {
+    Console.WriteLine("Наибольшее число будет: " + numbers3);
+  }
+}
